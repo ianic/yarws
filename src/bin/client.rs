0a9164b8eb8d8a144bba780288e83db552904c04
Igor Anic <ianic@inet.hr>
2020-04-23 15:39:23 +0200

    enable client to repeat call loop

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,10 +1,14 @@
 use structopt::StructOpt;
-use yarws::{connect, Error};
+use tokio::time::{delay_for, Duration};
+use yarws::{connect, log, Error};
 
 #[derive(StructOpt, Debug)]
 struct Args {
     #[structopt(default_value = "ws://127.0.0.1:9001")]
     url: String,
+
+    #[structopt(short = "r", long = "repeat", default_value = "0")] // <0 repeates forever
+    repeat: isize,
 }
 
 // send different sizes of text messages to the echo server
@@ -13,17 +17,27 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args = Args::from_args();
-    let mut socket = connect(&args.url, None).await?.into_text();
+    let mut repeat = args.repeat;
+    loop {
+        {
+            let mut socket = connect(&args.url, log::config()).await?.into_text();
 
-    let data = "01234567890abcdefghijklmnopqrstuvwxyz"; //36 characters
-    let sizes = vec![1, 36, 125, 126, 127, 65535, 65536, 65537, 1048576];
-    for size in sizes {
-        let rep = size / data.len() + 1;
-        let req = &data.repeat(rep)[0..size];
+            let data = "01234567890abcdefghijklmnopqrstuvwxyz"; //36 characters
+            let sizes = vec![1, 36, 125, 126, 127, 65535, 65536, 65537, 1048576];
+            for size in sizes {
+                let rep = size / data.len() + 1;
+                let req = &data.repeat(rep)[0..size];
 
-        socket.send(req).await?;
-        let rsp = socket.recv_one().await?;
-        assert_eq!(req, rsp);
+                socket.send(req).await?;
+                let rsp = socket.recv_one().await?;
+                assert_eq!(req, rsp);
+            }
+        }
+        if repeat == 0 {
+            break;
+        }
+        repeat -= 1;
+        delay_for(Duration::from_secs(1)).await;
     }
     Ok(())
 }
